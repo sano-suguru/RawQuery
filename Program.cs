dbee8868e29e18690a56fff4375c762d55758be6
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-09-26 23:07:39 +0900

    SQLInjectionとその対策

--- Program.cs
+++ Program.cs
@@ -1,12 +1,76 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
 
-namespace RawQuery
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace RawQuery {
+  class Program {
+    static void Main(string[] args) => 
+      SQLInjectionMeasures3();
+
+    static void ExecRawQuery() {
+      using (var context = new BlogContext()) {
+        var posts = context.Posts
+          .FromSql("select * from Posts") //=> `FromSql`での生クエリは、モデルのすべてのプロパティを返す必要があるためカラムを全て取得
+          .ToList();
+      }
     }
+
+    static void SqlInjection() {
+      using (var context = new BlogContext()) {
+        string category = "C#"; //=> 検索条件として渡されたと仮定
+
+#pragma warning disable EF1000 // Possible SQL injection vulnerability.
+        var posts = context.Posts
+          .FromSql("select * from Posts where Category = '" + category + "' and IsPrivate = 0")
+#pragma warning restore EF1000 // Possible SQL injection vulnerability.
+          .ToList();
+
+        foreach (var p in posts) {
+          Console.WriteLine($@"
+          {p.Title}
+          {p.Category}
+          {(p.IsPrivate ? "非公開" : "公開")}");
+        }
+      }
+    }
+
+    static void SQLInjectionMeasures1() {
+      using (var context = new BlogContext()) {
+        var category = new SqlParameter("category", "C#");
+
+        var posts = context.Posts
+          .FromSql("select * from Posts where Category = @category and IsPrivate = 0", category)
+          .ToList();
+      }
+    }
+
+    static void SQLInjectionMeasures2() {
+      using (var context = new BlogContext()) {
+        var category = "C#";
+
+        var posts = context.Posts
+          .FromSql("select * from Posts where Category = {0} and IsPrivate = 0", category)
+          .ToList();
+      }
+    }
+
+    static void SQLInjectionMeasures3() {
+      using (var context = new BlogContext()) {
+        var category = "C#";
+
+        var posts = context.Posts
+          .FromSql($"select * from Posts where Category = {category} and IsPrivate = 0")
+          .ToList();
+
+        foreach (var p in posts) {
+          Console.WriteLine($@"
+          {p.Title}
+          {p.Category}
+          {(p.IsPrivate ? "非公開" : "公開")}");
+        }
+      }
+    }
+  }
 }
+

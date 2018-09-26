95b1133642fdd9a1c4df97fd97cc6717974589a9
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-09-26 22:46:37 +0900

    エンティティとデータベースコンテキストを追加

---
+++ BlogContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace RawQuery {
+  class BlogContext : DbContext {
+    public DbSet<Post> Posts { get; set; }
+
+    static readonly ILoggerFactory loggerFactory = new LoggerFactory(new[] {
+        new ConsoleLoggerProvider((category, level) =>
+          category == DbLoggerCategory.Database.Command.Name
+              && level == LogLevel.Information, includeScopes: true) });
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+      optionsBuilder
+        .EnableSensitiveDataLogging()
+        .UseLoggerFactory(loggerFactory)
+        .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDB;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.Entity<Post>().HasData(
+          new Post {
+            ID = 1,
+            Title = "C# 8 ロードマップ",
+            Category = "C#",
+            IsPrivate = false
+          },
+          new Post {
+            ID = 2,
+            Title = "Array.some と Array.includes の使い分け",
+            Category = "JavaScript",
+            IsPrivate = false
+          },
+          new Post {
+            ID = 3,
+            Title = "秘密のポートフォリオ",
+            Category = "C#",
+            IsPrivate = true
+          });
+  }
+}

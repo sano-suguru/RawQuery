8091e007c06e94d5ab5685493e2f8e7062db4351
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-09-26 22:41:48 +0900

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RawQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
